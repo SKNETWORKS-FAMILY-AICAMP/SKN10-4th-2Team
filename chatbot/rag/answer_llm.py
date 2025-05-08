954e6903f7e143171ee92a08186a12fbc1552bab
seunggi-coding <moonsys010103@gmail.com>
2025-05-08 19:01:45 +0900

    - rag 프롬프트 수정정

--- chatbot/rag/answer_llm.py
+++ chatbot/rag/answer_llm.py
@@ -59,7 +59,7 @@ def llm_answer(prompt: str, category: str = None, history: list = None) -> str:
         "model": "gemma2-9b-it",
         "messages": messages,
         "temperature": 0.7,
-        "max_tokens": 1050
+        "max_tokens": 2000
     }
 
     response = requests.post(url, headers=headers, json=data)
