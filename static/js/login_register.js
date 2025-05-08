4a56a0182791e6a2025373ca052f7af50b118806
seunggi-coding <moonsys010103@gmail.com>
2025-05-08 20:18:38 +0900

    UI 병합

--- static/js/login_register.js
+++ static/js/login_register.js
@@ -1,11 +1,10 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpBtn = document.getElementById('signUp');
+const signInBtn = document.getElementById('signIn');
+const container = document.querySelector('.container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+signUpBtn.addEventListener('click', () => {
+	container.classList.add('right-panel-active');
 });
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInBtn.addEventListener('click', () => {
+	container.classList.remove('right-panel-active');
 });
