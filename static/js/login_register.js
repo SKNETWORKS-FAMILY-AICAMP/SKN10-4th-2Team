322d296a54a5564609a9a0bc9fa1001461858a19
shinminju <sinminju73@gmail.com>
2025-05-12 00:37:32 +0900

    전체적 ui 수정정

--- static/js/login_register.js
+++ static/js/login_register.js
@@ -1,10 +1,11 @@
-const signUpBtn = document.getElementById('signUp');
-const signInBtn = document.getElementById('signIn');
-const container = document.querySelector('.container');
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-signUpBtn.addEventListener('click', () => {
-	container.classList.add('right-panel-active');
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
 });
-signInBtn.addEventListener('click', () => {
-	container.classList.remove('right-panel-active');
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
